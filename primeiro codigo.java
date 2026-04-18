0d6b417aa4c9eb0cc937cf85270f4cb5febe1fe5
Fallen-star-cell <cristiane.carvalho80@gmail.com>
2026-04-18 10:54:32 -0300

    Createprimeiro codig o.java

---
+++ primeiro codigo.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		char caixa2 = 'G';         
+		int caixa = 16;
+		boolean caixa3 = true;
+		int laranja = 5;
+		int uvas = 10;
+		int soma = 	 uvas+laranja;
+		 
+		                  	}
+}
